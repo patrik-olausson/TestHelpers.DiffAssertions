7d2bf5a13d6192cf5d8d3226cb5ff6e104fab337
Patrik Olausson <patrik.olausson@ica.se>
2020-10-22 19:55:38 +0200

    Updates gitignore file to allow nUnit test file

---
+++ DiffAssertions.461.nUnitTests/SubFolder/FindCurrentDirectoryTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using TestHelpers.DiffAssertions;
+
+namespace DiffAssertions._461.nUnitTests.SubFolder
+{
+    [TestFixture]
+    public class FindCurrentDirectoryTests
+    {
+        [Test]
+        public void WhenRunningTestItIsPossibleToDetermineTheCorrectRootPaths()
+        {
+            //This works because the NUnitDiffAssertionsSetup class sets the working directory...
+            DiffAssert
+                .ThatContentsOf("SubFolder/IsItPossibleToFindOutTheRootFolderEvenWithNUnit")
+                .Equals("Yeah! It is possible!");
+        }
+    }
+}
