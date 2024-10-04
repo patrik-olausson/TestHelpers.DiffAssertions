bc9ce5e37899b5a39f257c0f4c28debc1a9364a1
Patrik Olausson <patrik.olausson@ica.se>
2024-10-04 11:37:00 +0200

    Adds test harness base class with support for handling files.

---
+++ DiffAssertions.Tests/Utils/FileHelperTests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using FluentAssertions;
+using TestHelpers.DiffAssertions.Utils;
+using Xunit;
+
+namespace DiffAsserterTests.Utils.FilHelperTests
+{
+    public class ExtractDirectoryNameFromPath : FileHelperTestHarness
+    {
+        [Theory]
+        [InlineData(@"\SomeFolder\SomeSubFolder\SomeFile.txt", "/SomeFolder/SomeSubFolder")]
+        [InlineData("/SomeFolder/SomeSubFolder/SomeFile.txt", "/SomeFolder/SomeSubFolder")]
+        [InlineData("SomeFolder/SomeSubFolder/SomeFile.txt", "SomeFolder/SomeSubFolder")]
+        [InlineData(null, "")]
+        [InlineData("", "")]
+        [InlineData(" ", "")]
+        void GivenDifferentPaths_ThenItReturnsTheExpectedCleanedPath(
+            string path,
+            string expectedCleanedPath)
+        {
+            var result = FileHelper.ExtractDirectoryNameFromPath(path);
+            
+            AssertPath(expectedCleanedPath, result);
+        }
+    }
+
+    public class GetRelativePathToCurrentTestDirectory : FileHelperTestHarness
+    {
+        [Theory]
+        [InlineData(
+            @"C:\RootFolder\SubFolder\SolutionFolder\TestProjectFolder\SubFolder1\SubFolder2\SubFolder3\NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "SubFolder1/SubFolder2/SubFolder3")]
+        [InlineData(
+            @"C:\RootFolder\TestProjectFolder\SubFolder1\NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "SubFolder1")]
+        [InlineData(
+            @"C:\TestProjectFolder\NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "")]
+        [InlineData(
+            "C:/RootFolder/SubFolder/SolutionFolder/TestProjectFolder/SubFolder1/SubFolder2/SubFolder3/NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "SubFolder1/SubFolder2/SubFolder3")]
+        [InlineData(
+            "C:/RootFolder/TestProjectFolder/SubFolder1/NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "SubFolder1")]
+        [InlineData(
+            "C:/TestProjectFolder/NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "")]
+        public void GivenDifferentFilePathsAndTestDirectoryNames_ThenItExtractsTheExpectedRelativePath(
+            string callerFilePath,
+            string testProjectName,
+            string expectedRelativePath)
+        {
+            var result = FileHelper.GetRelativePathToCurrentTestDirectory(callerFilePath, testProjectName);
+            
+            AssertPath(expectedRelativePath, result);
+        }
+
+        [Fact]
+        public void GivenCallerFilePathThatDoesNotContainTheSpecifiedTestProjectName_ThenItThrowsAnException()
+        {
+            var callerFilePath = "C:/RootFolder/TestProjectFolder/SubFolder1/NameOfFileThatCallsMethod.cs";
+            var testProjectName = "NameThatIsNotPartOfTheFilePath";
+            
+            Assert.Throws<ArgumentException>(() => FileHelper.GetRelativePathToCurrentTestDirectory(callerFilePath, testProjectName));
+        }
+    }
+    
+    public class GetRelativePathToFileInTestProject : FileHelperTestHarness
+    {
+        [Theory]
+        [InlineData(
+            @"C:\RootFolder\SubFolder\SolutionFolder\TestProjectFolder\SubFolder1\SubFolder2\SubFolder3\NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "CustomFilename.txt",
+            "SubFolder1/SubFolder2/SubFolder3/CustomFilename.txt")]
+        [InlineData(
+            @"C:\RootFolder\TestProjectFolder\SubFolder1\NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "CustomFilename.txt",
+            "SubFolder1/CustomFilename.txt")]
+        [InlineData(
+            @"C:\TestProjectFolder\NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "CustomFilename.txt",
+            "CustomFilename.txt")]
+        [InlineData(
+            "C:/RootFolder/SubFolder/SolutionFolder/TestProjectFolder/SubFolder1/SubFolder2/SubFolder3/NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "CustomFilename.txt",
+            "SubFolder1/SubFolder2/SubFolder3/CustomFilename.txt")]
+        [InlineData(
+            "C:/RootFolder/TestProjectFolder/SubFolder1/NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "CustomFilename.txt",
+            "SubFolder1/CustomFilename.txt")]
+        [InlineData(
+            "C:/TestProjectFolder/NameOfFileThatCallsMethod.cs", 
+            "TestProjectFolder",
+            "CustomFilename.txt",
+            "CustomFilename.txt")]
+        public void GivenDifferentFilePathsAndTestDirectoryNames_ThenItExtractsTheRelativePathAndCombinesItWithTheFilenameAsExpected(
+            string callerFilePath,
+            string testProjectName,
+            string filename,
+            string expectedRelativePath)
+        {
+            var result = FileHelper.GetRelativePathToFileInTestProject(
+                callerFilePath,
+                testProjectName,
+                filename);
+            
+            AssertPath(expectedRelativePath, result);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void GivenThatTheFilenameIsNotSpecified_ThenItThrowsAnArgumentException(string filename)
+        {
+            Assert.Throws<ArgumentException>(() => FileHelper.GetRelativePathToFileInTestProject("C:/TestProjectFolder/NameOfFileThatCallsMethod.cs", "TestProjectFolder", filename));
+        }
+    }
+
+    public class GetAbsolutePathToFile : FileHelperTestHarness
+    {
+        [Theory]
+        [InlineData(
+            "C:/TestProjectFolder/NameOfFileThatCallsMethod.cs", 
+            "CustomFilename.txt",
+            "C:/TestProjectFolder/CustomFilename.txt")]
+        [InlineData(
+            "C:/TestProjectFolder/NameOfFileThatCallsMethod.cs", 
+            "SubFolder/CustomFilename.txt",
+            "C:/TestProjectFolder/SubFolder/CustomFilename.txt")]
+        public void GivenAbsolutePathToAFileAndASpecifiedFilename_ThenItReplacesTheFilenameAsExpected(
+            string pathToReferenceFile,
+            string filenameThatCouldContainSubdirectories,
+            string expectedAbsolutePath)
+        {
+            var result = FileHelper.GetAbsolutePathToFile(pathToReferenceFile, filenameThatCouldContainSubdirectories);
+            
+            AssertPath(expectedAbsolutePath, result);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void GivenThatTheCallerFilePathIsInvalid_ThenItThrowsAnArgumentException(string callerFilePath)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => FileHelper.GetAbsolutePathToFile(callerFilePath, "CustomFilename.txt"));
+            ex.Message.Should().StartWith("Not possible to extract");
+        }
+        
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void GivenThatTheFilenameIsInvalid_ThenItThrowsAnArgumentException(string filename)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => FileHelper.GetAbsolutePathToFile("C:/TestProjectFolder/NameOfFileThatCallsMethod.cs", filename));
+            ex.Message.Should().StartWith("Value cannot be null or whitespace.");
+        }
+    }
+
+    public class GetRelativePath : FileHelperTestHarness
+    {
+        [Theory]
+        [InlineData(
+            @"C:\RootFolder1\SubFolder\SolutionFolder\TestProjectFolder\SubFolder1\SubFolder2\SubFolder3\NameOfFileThatCallsMethod.cs",
+            "TestProjectFolder",
+            "SubFolder1/SubFolder2/SubFolder3/NameOfFileThatCallsMethod.cs")]
+        [InlineData(
+            @"C:\RootFolder2\SubFolder\SolutionFolder\TestProjectFolder\SubFolder1\SubFolder2\SubFolder3", 
+            "TestProjectFolder",
+            "SubFolder1/SubFolder2/SubFolder3")]
+        public void GivenAbsolutePathToAFileOrDirectoryAndASpecifiedRootDirectory_ThenItReplacesTheFilenameAsExpected(
+            string absolutePathToFileOrDirectory,
+            string rootDirectoryName,
+            string expectedRelativePath)
+        {
+            var result = FileHelper.GetRelativePath(absolutePathToFileOrDirectory, rootDirectoryName);
+            
+            AssertPath(expectedRelativePath, result);
+        }
+    }
+
+    public class FileHelperTestHarness
+    {
+        protected void AssertPath(string expectedPath, string actualPath)
+        {
+            //Make sure test is able to run on different OS
+            actualPath = GetPathThatWorksOnDifferentOperatingSystems(actualPath);
+            
+            Assert.Equal(expectedPath, actualPath);
+        }
+        
+        private string GetPathThatWorksOnDifferentOperatingSystems(string path)
+        {
+            const char WindowsDirectorySeparator = '\\';
+            const char UnixDirectorySeparator = '/';
+
+            return path.Replace(WindowsDirectorySeparator, UnixDirectorySeparator);
+        }
+    }
+};
---
+++ DiffAssertions/Public/DiffAssertTestHarness.cs
@@ -0,0 +1,128 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using TestHelpers.DiffAssertions.Utils;
+
+namespace TestHelpers.DiffAssertions
+{
+    /// <summary>
+    /// Base class that should simplify the use of DiffAssertions in test harnesses.
+    /// </summary>
+    public class DiffAssertTestHarness
+    {
+        /// <summary>
+        /// Helper method that makes it easier to use DiffAssertions.
+        /// </summary>
+        /// <param name="testProjectDirectoryName">The name of the test project directory (normally the same as the test project).
+        /// The name is used to be able to infer the root directory of files on disc relative to the test project.</param>
+        /// <param name="fileNameForExpectedValue">The name of the file, without any path and extensions.
+        /// Path to the directory and the .expected.txt suffix will be added automatically.
+        /// </param>
+        /// <param name="actualValue">The value produced by the current version of the code.</param>
+        /// <param name="callerFilePath">The absolute path to a file that is in the root directory you want to use
+        /// (normally the directory of the test that is currently executing).
+        /// The best way is to rely on using the CallerFilePath attribute to capture a call from the test that is
+        /// currently executing and reference files in the same directory or a subdirectory structure.</param>
+        protected virtual void DiffAssert(
+            string testProjectDirectoryName,
+            string fileNameForExpectedValue,
+            string actualValue,
+            [CallerFilePath] string callerFilePath = "")
+        {
+            var pathToRootFolder = GetRelativePathToFolder(callerFilePath, testProjectDirectoryName);
+            
+            DiffAssertions.DiffAssert
+                .ThatContentsOf(Path.Combine(pathToRootFolder, fileNameForExpectedValue))
+                .Equals(actualValue);
+        }
+
+        /// <summary>
+        /// Gets the relative path to a folder using the test project directory as root folder.
+        /// </summary>
+        /// <param name="callerFilePath">The absolute path to a file that is in the root directory you want to use
+        /// (normally the directory of the test that is currently executing).
+        /// The best way is to rely on using the CallerFilePath attribute to capture a call from the test that is
+        /// currently executing and reference files in the same directory or a subdirectory structure.</param>
+        /// <param name="testProjectDirectoryName">The name of the test project directory (normally the same as the test project).
+        /// The name is used to be able to infer the root directory of files on disc relative to the test project.</param>
+        protected string GetRelativePathToFolder(string callerFilePath, string testProjectDirectoryName)
+        {
+            return FileHelper.GetRelativePathToCurrentTestDirectory(callerFilePath, testProjectDirectoryName);
+        }
+
+        /// <summary>
+        /// Gets the relative path to a file using the test project director as root folder.
+        /// </summary>
+        /// <param name="callerFilePath">The absolute path to a file that is in the root directory you want to use
+        /// (normally the directory of the test that is currently executing).
+        /// The best way is to rely on using the CallerFilePath attribute to capture a call from the test that is
+        /// currently executing and reference files in the same directory or a subdirectory structure.</param>
+        /// <param name="testProjectDirectoryName">The name of the test project directory (normally the same as the test project).
+        /// The name is used to be able to infer the root directory of files on disc relative to the test project.</param>
+        /// <param name="filename">The name of the file you want to append to the path.
+        /// This could (and probably should) include a path to a subdirectory structure.
+        /// NOTE: when using subdirectories in the path, use / to ensure it will work for both Windows and Linux.</param>
+        /// <returns></returns>
+        protected string GetRelativePathToFile(string callerFilePath, string testProjectDirectoryName, string filename)
+        {
+            return FileHelper.GetRelativePathToFileInTestProject(
+                callerFilePath,
+                testProjectDirectoryName,
+                filename);
+        }
+
+        /// <summary>
+        /// Gets information about a specified file.
+        /// </summary>
+        /// <param name="testProjectDirectoryName">The name of the test project directory (normally the same as the test project).
+        /// The name is used to be able to infer the root directory of files on disc relative to the test project.</param>
+        /// <param name="filename">The name of the file you want to append to the path.
+        /// This could (and probably should) include a path to a subdirectory structure.
+        /// NOTE: when using subdirectories in the path, use / to ensure it will work for both Windows and Linux.</param>
+        /// <param name="directory">Path from the relative root directory.</param>
+        /// <param name="fileExtension">File extension (.json, .txt, etc)</param>
+        /// <param name="callerFilePath">The absolute path to a file that is in the root directory you want to use
+        /// (normally the directory of the test that is currently executing).
+        /// The best way is to rely on using the CallerFilePath attribute to capture a call from the test that is
+        /// currently executing and reference files in the same directory or a subdirectory structure.</param>
+        /// <returns></returns>
+        public FileInfo GetFileInfo(
+            string testProjectDirectoryName,
+            string filename,
+            string directory = "[TestFiles]",
+            string fileExtension = ".json",
+            [CallerFilePath] string callerFilePath = "")
+        {
+            var filePath = GetRelativePathToFile(
+                callerFilePath,
+                testProjectDirectoryName,
+                Path.Combine(directory, $"{filename}{fileExtension}"));
+
+            return new FileInfo(filePath);
+        }
+
+        /// <summary>
+        /// Reads all text from a file and adds error message that should be helpful when the file is not found.
+        /// </summary>
+        /// <param name="testProjectDirectoryName"></param>
+        /// <param name="filename">The name of the file you want to append to the path.
+        /// This could (and probably should) include a path to a subdirectory structure.
+        /// NOTE: when using subdirectories in the path, use / to ensure it will work for both Windows and Linux.</param>
+        /// <param name="callerFilePath">The absolute path to a file that is in the root directory you want to use
+        /// (normally the directory of the test that is currently executing).
+        /// The best way is to rely on using the CallerFilePath attribute to capture a call from the test that is
+        /// currently executing and reference files in the same directory or a subdirectory structure.</param>
+        /// <returns></returns>
+        protected string GetContentFromFile(
+            string testProjectDirectoryName,
+            string filename,
+            [CallerFilePath] string callerFilePath = "")
+        {
+            var filePath = GetRelativePathToFile(
+                callerFilePath,
+                testProjectDirectoryName,
+                filename);
+            
+            return FileHelper.GetContentFromFile(filePath);
+        }
+    }
+}
